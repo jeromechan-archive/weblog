8e71045e626c2beb8ba07ede75fb3e0b6e23bd9d
chenjinlong <chenjinlong@promote.cache-dns.local>
2014-06-30 00:13:01 +0800

    停止加载google-fonts

--- wp-content/themes/inove/functions.php
+++ wp-content/themes/inove/functions.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Class Disable_Google_Fonts
+ * 停止加载google-fonts
+ *
+ * @author chenjinlong 20140630
+ */
+class Disable_Google_Fonts {
+    public function __construct() {
+        add_filter( 'gettext_with_context', array( $this, 'disable_open_sans' ), 888, 4 );
+    }
+    public function disable_open_sans( $translations, $text, $context, $domain ) {
+        if ( 'Open Sans font: on or off' == $context && 'on' == $text ) {
+            $translations = 'off';
+        }
+        return $translations;
+    }
+}
+$disable_google_fonts = new Disable_Google_Fonts;
+
 /** inove options */
 class iNoveOptions {
 
