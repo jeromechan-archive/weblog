528600a5d938b13feaed056f5c5a7ac2e4f57d23
chenjinlong <chenjinlong@promote.cache-dns.local>
2014-06-29 21:48:16 +0800

    Upgrade wp to v3.9.1

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-header("Content-Type: text/html; charset=UTF-8");
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
