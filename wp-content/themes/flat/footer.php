0a37b0aa3583c3fd468ec35bce6b5559fe87c8c2
chenjinlong <chenjinlong@promote.cache-dns.local>
2014-06-29 22:03:02 +0800

    update the track codes

--- wp-content/themes/flat/footer.php
+++ wp-content/themes/flat/footer.php
@@ -56,10 +56,5 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/javascript">
-    var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-    document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F02398c2183303e5db981f815f39dcafd' type='text/javascript'%3E%3C/script%3E"));
-</script>
-
 </body>
 </html>
--- wp-content/themes/inove/footer.php
+++ wp-content/themes/inove/footer.php
@@ -55,6 +55,12 @@
 	}
 ?>
 
+    <!--百度统计 BEGIN-->
+    <script type="text/javascript">
+        var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+        document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F02398c2183303e5db981f815f39dcafd' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <!--百度统计 END-->
 </body>
 </html>
 
