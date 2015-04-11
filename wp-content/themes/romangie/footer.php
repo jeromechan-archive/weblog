b5d2dc708530c8dbcb901c40ad2c153a4ffd2681
jeromechan <jerome.chan369@gmail.com>
2015-04-11 21:31:22 +0800

    升级4.1.1版本+整理无效废弃目录

--- wp-content/themes/romangie/footer.php
+++ wp-content/themes/romangie/footer.php
@@ -17,5 +17,24 @@
 	</div>
 </div> <!-- /footer -->
 <?php wp_footer(); ?>
+
+<!-- 加载jquery.lazyload plugin. added by chenjinlong -->
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.lazyload.min.js"></script>
+<script type="text/javascript">
+    $(function() {
+        $("img").lazyload({
+                "effect" : "fadeIn",
+                "placeholder" : "<?php bloginfo('template_url'); ?>/images/loader.gif",
+                "threshold" : 200
+            }
+        );
+    });
+</script>
+
+<!--站长统计 BEGIN added by chenjinlong-->
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1252898702'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1252898702%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+<!--站长统计 END-->
+
 </body>
 </html>
